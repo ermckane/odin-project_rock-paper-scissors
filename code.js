42091e29f076dee071056778672998d4b47601a4
unknown <ermckane@gmail.com>
2022-09-10 15:26:50 -0400

    Finished debugging single round play

--- code.js
+++ code.js
@@ -4,37 +4,30 @@ let computerNumber;
 let computerSelection;
 let playerSelection;
 
-function game() {
-    getPlayerSelection();
-    getCompSelection();
-    playRound();
-}
-
+//Retrieve player input and convert so always capital first letter followed by lowercase
 function getPlayerSelection() {
     playerAnswer = prompt('Select either Rock, Paper, or Scissors in order to save humanity!');
     playerSelection = playerAnswer.charAt(0).toUpperCase() + playerAnswer.slice(1).toLowerCase();
-    console.log(playerSelection);
     return playerSelection;
     }
 
+//Get random selection by converting random numbers to choice
 function getCompSelection() {
-    computerNumber = Math.floor((Math.random() * 100) + 1);
-    if (computerNumber < 33) {
+    computerNumber = Math.floor((Math.random() * 100) + 1);      //get random number
+    if (computerNumber < 33) {                               
         computerSelection = 'Rock';
-        console.log(computerSelection);
         return computerSelection;
-    } else if (computerNumber > 33 && computerSelection < 67) {
+    } else if (computerNumber > 67) {
         computerSelection = 'Paper';
-        console.log(computerSelection);
         return computerSelection;
     } else {
         computerSelection = 'Scissors';
-        console.log(computerSelection);
         return computerSelection;
     }
 } 
 
-function playRound(playerSelection,) {
+//Compare computerSelection to playerSelection to get results
+function playRound() {
     if ((playerSelection == 'Rock' && computerSelection == 'Scissors') ||
         (playerSelection == 'Scissors' && computerSelection == 'Paper') ||
         (playerSelection == 'Paper' && computerSelection == 'Rock')) {
@@ -49,4 +42,11 @@ function playRound(playerSelection,) {
             result = 'It\'s a tie! Try again losers!';
             return result;
     } 
+}
+
+function game() {
+    getPlayerSelection();
+    getCompSelection();
+    playRound();
+    return result;
 }
